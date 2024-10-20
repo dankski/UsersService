1c2d6f503e186bba8b85fc73ca9ad30a065b4bc5
Giacomo Amoroso <giacomo@amoroso.me>
2024-10-20 21:26:11 +0200

    WIP 121 added service

---
+++ src/main/java/com/appsdeveloperblog/estore/UsersService/query/UserEventsHandler.java
@@ -0,0 +1,39 @@
+package com.appsdeveloperblog.estore.UsersService.query;
+
+
+import org.axonframework.queryhandling.QueryHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.appsdeveloperblog.estore.core.model.FetchUserPaymentDetailsQuery;
+import com.appsdeveloperblog.estore.core.model.PaymentDetails;
+import com.appsdeveloperblog.estore.core.model.User;
+
+@Component
+public class UserEventsHandler {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserEventsHandler.class);
+
+  @QueryHandler
+  public User findUser(FetchUserPaymentDetailsQuery query) {
+    PaymentDetails paymentDetails = PaymentDetails.builder()
+    .cardNumber("123Card")
+    .cvv("123")
+    .name("SERGEY KARGOPOLOV")
+    .validUntilMonth(12)
+    .validUntilYear(2030)
+    .build();
+     
+    User user = User.builder()
+    .firstName("Sergey")
+    .lastName("Kargopolov")
+    .userId(query.getUserId())
+    .paymentDetails(paymentDetails)
+    .build();
+
+    LOGGER.info("WIP and needs some more love");
+
+    return user;
+  }
+}
